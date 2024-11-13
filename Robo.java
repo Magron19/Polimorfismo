252146dcfba09f920d090d8abd78d4eea202aaff
Magron19 <142603206+Magron19@users.noreply.github.com>
2024-11-13 20:25:35 -0300

    Delete Robo.java

--- Robo.java
+++
@@ -1,7 +0,0 @@
-// Classe Robo que implementa a interface FazSom
-public class Robo implements FazSom {
-    @Override
-    public void emitirSom() {
-        System.out.println("Beep beep!");  // Som do robô
-    }
-}
