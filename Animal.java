5c22ef087bb16d1d8e2e9bdba3564114194b5bb6
Magron19 <142603206+Magron19@users.noreply.github.com>
2024-11-13 20:24:44 -0300

    Delete Animal.java

--- Animal.java
+++
@@ -1,5 +0,0 @@
-// Definindo a classe base Animal com polimorfismo por herança
-public abstract class Animal {
-    // Método abstrato para o som que cada animal vai fazer
-    abstract void fazerSom();
-}
