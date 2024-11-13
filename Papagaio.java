db60293c05504763eb96b0e533a0c21eae55885b
Magron19 <142603206+Magron19@users.noreply.github.com>
2024-11-13 20:25:29 -0300

    Delete Papagaio.java

--- Papagaio.java
+++
@@ -1,7 +0,0 @@
-// Classe Papagaio que herda de Animal e implementa o método fazerSom()
-public class Papagaio extends Animal {
-    @Override
-    public void fazerSom() {
-        System.out.println("Loro!!!");  // Som do papagaio
-    }
-}
