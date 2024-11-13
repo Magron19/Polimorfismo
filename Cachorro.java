852e763784c3a78184c9373137543ed4f4c623bc
Magron19 <142603206+Magron19@users.noreply.github.com>
2024-11-13 20:24:51 -0300

    Delete Cachorro.java

--- Cachorro.java
+++
@@ -1,7 +0,0 @@
-// Classe Cachorro que herda de Animal e implementa o método fazerSom()
-public class Cachorro extends Animal {
-    @Override
-    public void fazerSom() {
-        System.out.println("Au au!");  // Som do cachorro
-    }
-}
