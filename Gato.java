c546f56defd5e32d2ef67a7854e69c18c6b46a0e
Magron19 <142603206+Magron19@users.noreply.github.com>
2024-11-13 20:25:18 -0300

    Delete Gato.java

--- Gato.java
+++
@@ -1,7 +0,0 @@
-// Classe Gato que herda de Animal e implementa o método fazerSom()
-public class Gato extends Animal {
-    @Override
-    public void fazerSom() {
-        System.out.println("Miau!");  // Som do gato
-    }
-}
